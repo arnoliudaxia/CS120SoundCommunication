2351841423d5bf430e41637b43c4531f4dfd26c7
Liuyifei <liuyf7@shanghaitech.edu.cn>
2022-09-08 13:44:35 +0800

    简单放正弦波

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,18 @@
 
 import dataAgent.SoundUtil;
-import dataAgent.StorgePolicy;
 
-import java.io.*;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    static void threadBlockTime(int timems)
-    {
+    static void threadBlockTime(int timems) {
         try {
             Thread.sleep(timems);
         } catch (InterruptedException e) {
             System.exit(1);
         }
     }
+
     public static void main(final String[] args) throws IOException {
 
         final AudioHw audiohw = new AudioHw();
@@ -28,37 +27,57 @@ public class Main {
         //#endregion
         if (taskchoice == 1) {
             //record 10s，然后回放
-            final int recordTime=10;
+            final int recordTime = 10;
             System.out.println("Recording 10s...");
             audiohw.isRecording = true;
-            threadBlockTime(recordTime*1000);
+            threadBlockTime(recordTime * 1000);
             audiohw.isRecording = false;
             System.out.println("播放录音");
-            audiohw.playQueue=audiohw.dataagent.retriveData(Config.HW_BUFFER_SIZE);
+            audiohw.playQueue = audiohw.dataagent.retriveData(Config.HW_BUFFER_SIZE);
             audiohw.isPlay = true;
-            threadBlockTime(recordTime*1000);
+            threadBlockTime(recordTime * 1000);
             audiohw.isPlay = false;
 
         }
-        if(taskchoice==2)
-        {
+        if (taskchoice == 2) {
             //同时放声音和录音10s，然后回放
-            final int recordTime=10;
-            audiohw.playSound(SoundUtil.playsoundFile("res\\cai.dat",Config.HW_BUFFER_SIZE));
-            audiohw.isPlay=true;
+            final int recordTime = 10;
+            audiohw.playSound(SoundUtil.playsoundFile("res\\cai.dat", Config.HW_BUFFER_SIZE));
+            audiohw.isPlay = true;
             System.out.println("放音乐中...");
             System.out.println("Recording 10s...");
             audiohw.isRecording = true;
-            threadBlockTime(recordTime*1000);
-            audiohw.isPlay= false;
+            threadBlockTime(recordTime * 1000);
+            audiohw.isPlay = false;
             audiohw.isRecording = false;
             System.out.println("播放录音");
             audiohw.playSound(audiohw.dataagent.retriveData(Config.HW_BUFFER_SIZE));
             audiohw.isPlay = true;
-            threadBlockTime(recordTime*1000);
+            threadBlockTime(recordTime * 1000);
             audiohw.isPlay = false;
 
 
+        }
+        if (taskchoice == 3) {
+            //生成一个频率为1000Hz的正弦波，采样率是44100Hz
+
+
+//            audiohw.playRawData(SoundUtil.generateSinwave(1000,2,Config.PHY_TX_SAMPLING_RATE));
+            float phase = 0;
+            float freq = 5000f;  // Hz
+            float sampleRate = 48000f;
+            float dphase = (2 * (float) Math.PI * freq) / sampleRate;
+            float[] soundTemp = new float[Config.HW_BUFFER_SIZE];
+            for (int i = 0; i < Config.HW_BUFFER_SIZE; i++) {
+                phase = phase + dphase;
+                soundTemp[i] = (float) (Math.sin((double) phase));  // sine wave
+            }
+            audiohw.playRawData(soundTemp);
+
+            audiohw.isPlay = true;
+            audiohw.isLoop = true;
+            threadBlockTime(1000);
+            audiohw.isPlay = false;
         }
         audiohw.stop();
 
