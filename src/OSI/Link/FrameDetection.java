6a354d8cc9aea3183123b8a54654cdb09b62019f
Liuyifei <liuyf7@shanghaitech.edu.cn>
2022-10-23 20:02:09 +0800

    FrameDetection接口

---
+++ src/OSI/Link/FrameDetection.java
@@ -0,0 +1,10 @@
+package OSI.Link;
+
+public interface FrameDetection
+{
+    /**
+     * 判断data数组里是否**可能**存在一个frame
+     * @return 如果可能存在返回True
+     */
+    public boolean detectPossibelFrame(float[] data);
+}
