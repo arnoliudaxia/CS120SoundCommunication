36dbc8cb1336fbe4dd6e751a93349e4a957a50ca
Liuyifei <liuyf7@shanghaitech.edu.cn>
2022-10-25 20:16:31 +0800

    读文本文件封装进smartConvertor

--- src/utils/smartConvertor.java
+++ src/utils/smartConvertor.java
@@ -1,5 +1,10 @@
 package utils;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class smartConvertor {
     public static float[] doubleToFloatArray(double[] input)
     {
@@ -18,5 +23,24 @@ public class smartConvertor {
             output[i]=(double) input[i];
         }
         return output;
+    }
+
+    /**
+     * 如果数据以二进制形式存储在一个文本文件的一行，请调用我嘿嘿
+     * @param path 文本文件路径
+     * @return 一个ArrayList<Integer>
+     * @throws FileNotFoundException
+     */
+    public static ArrayList<Integer> binInTextFile(String path) throws FileNotFoundException {
+        ArrayList<Integer> rawdata = new ArrayList<>();
+        File f = new File(path);
+        Scanner sc = new Scanner(f);
+        var rawstring = sc.nextLine();
+        for (int i = 0; i < rawstring.length(); i++) {
+            {
+                rawdata.add(Integer.parseInt(String.valueOf(rawstring.charAt(i))));
+            }
+        }
+        return rawdata;
     }
 }
