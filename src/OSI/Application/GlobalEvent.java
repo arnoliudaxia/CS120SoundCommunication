1b92b21e9ec971a3012ca2989310be7ec374f2cd
Liuyifei <liuyf7@shanghaitech.edu.cn>
2022-10-26 00:24:54 +0800

    二进制读写整合进入smartConvertor

--- src/OSI/Application/GlobalEvent.java
+++ src/OSI/Application/GlobalEvent.java
@@ -4,4 +4,5 @@ public class GlobalEvent {
     public static final Object SENDTASKOVER = new Object();
     public static final Object ALL_DATA_Recieved = new Object();
     public static final Object CONNECTED = new Object();
+    public static final Object SEND_SUCCESS = new Object();
 }
--- src/OSI/Application/MessageSender.java
+++ src/OSI/Application/MessageSender.java
@@ -2,7 +2,9 @@ package OSI.Application;
 
 import OSI.MAC.MACLayer;
 import utils.DebugHelper;
+import utils.smartConvertor;
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 public class MessageSender {
@@ -28,6 +30,10 @@ public class MessageSender {
     public void sendBinary(ArrayList<Integer> input) {
         System.out.println("发送二进制数据包");
         MACLayer.macBufferController.trySend(input);
+    }
+    public void sendFile(String path) throws FileNotFoundException {
+        DebugHelper.log("发送文件: "+path);
+        MACLayer.macBufferController.trySend(smartConvertor.binInFile(path));
     }
 
 }
