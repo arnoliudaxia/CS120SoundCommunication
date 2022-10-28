01a7a4b73d473b16e34e375353a9735aa618206b
Liuyifei <liuyf7@shanghaitech.edu.cn>
2022-10-28 21:40:34 +0800

    Debuger输出带毫秒数

--- src/utils/DebugHelper.java
+++ src/utils/DebugHelper.java
@@ -3,7 +3,7 @@ package utils;
 import java.text.SimpleDateFormat;
 
 public class DebugHelper {
-    static SimpleDateFormat df = new SimpleDateFormat("mm:ss");
+    static SimpleDateFormat df = new SimpleDateFormat("mm:ss.SSS");
     public static <T> void log(T message) {
         System.out.println(df.format(System.currentTimeMillis())+": "+message);
     }
