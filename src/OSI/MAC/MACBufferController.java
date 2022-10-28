a474dc3e69d61f6d1f7a39578d0b617ef3d1163b
Liuyifei <liuyf7@shanghaitech.edu.cn>
2022-10-28 21:57:35 +0800

    优化

--- src/OSI/MAC/MACBufferController.java
+++ src/OSI/MAC/MACBufferController.java
@@ -7,7 +7,11 @@ import OSI.Link.frameConfig;
 import utils.DebugHelper;
 import utils.smartConvertor;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 
 public class MACBufferController {
     BitPacker bitPacker = null;
