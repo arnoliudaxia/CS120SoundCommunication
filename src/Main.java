e4cbc899193feceb763e0e1410a3a6b483fb8452
Liuyifei <liuyf7@shanghaitech.edu.cn>
2022-11-09 23:47:53 +0800

    6800

--- src/Main.java
+++ src/Main.java
@@ -63,7 +63,7 @@ public class Main {
                     synchronized (GlobalEvent.ALL_DATA_Recieved) {
                         GlobalEvent.ALL_DATA_Recieved.wait();
                     }
-                    if(MACLayer.macBufferController.upStreamQueue.size()>=5100/MACFrame.SEGEMENT[3])
+                    if(MACLayer.macBufferController.upStreamQueue.size()>=6800/MACFrame.SEGEMENT[3])
                     {
                         DebugHelper.log("切换到3");
                         taskchoice=3;
@@ -134,7 +134,7 @@ public class Main {
                     synchronized (GlobalEvent.ALL_DATA_Recieved) {
                         GlobalEvent.ALL_DATA_Recieved.wait(4000);
                     }
-                    if(MACLayer.macBufferController.upStreamQueue.size()>=5100/MACFrame.SEGEMENT[3]*2-1)
+                    if(MACLayer.macBufferController.upStreamQueue.size()>=6800/MACFrame.SEGEMENT[3]*2-1)
                     {
                         MACLayer.macStateMachine.TxPending = true;
                         DebugHelper.log("数据接收全部完成");
