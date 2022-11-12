8fc18bbba0444d7e81952917f8526e61911a0bd9
Liuyifei <liuyf7@shanghaitech.edu.cn>
2022-11-12 22:36:52 +0800

    生成脚本

--- src/Tester.java
+++ src/Tester.java
@@ -1,9 +1,24 @@
-import OSI.Link.BitPacker;
-import com.github.psambit9791.wavfile.WavFileException;
+import utils.smartConvertor;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Tester {
-    public static void main(final String[] args) throws IOException, WavFileException {
+    public static void main(final String[] args) throws IOException {
+        ArrayList<Integer> data=new ArrayList<>();
+        //random 0 or 1
+        for (int i=0;i<6250*8/2;i++)
+        {
+            boolean choice=Math.random()>0.5;
+            if(choice){
+                data.add(1);
+                data.add(0);
+            }
+            else{
+                data.add(0);
+                data.add(1);
+            }
+        }
+        smartConvertor.binToFile("res\\INPUT_6250.bin",data);
     }
 }
