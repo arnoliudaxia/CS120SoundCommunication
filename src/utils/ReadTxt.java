b12aeb84d9fc9615babf76c450e59ae213f705a7
Liuyifei <liuyf7@shanghaitech.edu.cn>
2022-11-21 23:12:54 +0800

    readtext变为static

--- src/utils/ReadTxt.java
+++ src/utils/ReadTxt.java
@@ -5,13 +5,13 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class ReadTxt {
-    public String readTxt(String path) throws IOException {
+    public static String readTxt(String path) throws IOException {
         //read file
         //        System.out.println(data);
         return Files.readString(Paths.get(path));
     }
 
-    public byte[] readTxtBytes(String path) throws IOException {
+    public static byte[] readTxtBytes(String path) throws IOException {
         return Files.readAllBytes(Paths.get(path));
     }
 }
