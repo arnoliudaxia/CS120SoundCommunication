4501310b5245b5d3b801fd95f9baeca361a7fbcd
Liuyifei <liuyf7@shanghaitech.edu.cn>
2022-11-22 22:35:07 +0800

    打印彩色

--- src/Tester.java
+++ src/Tester.java
@@ -1,24 +1,11 @@
-import utils.smartConvertor;
+import utils.DebugHelper;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 public class Tester {
     public static void main(final String[] args) throws IOException {
-        ArrayList<Integer> data=new ArrayList<>();
-        //random 0 or 1
-        for (int i=0;i<6250*8/2;i++)
-        {
-            boolean choice=Math.random()>0.5;
-            if(choice){
-                data.add(1);
-                data.add(0);
-            }
-            else{
-                data.add(0);
-                data.add(1);
-            }
-        }
-        smartConvertor.binToFile("res\\INPUT_6250.bin",data);
+//        System.out.print("\33[31m 文字"+"\33[m\n");
+        DebugHelper.logColorful("文字", DebugHelper.printColor.RED);
+        DebugHelper.log("文字123");
     }
 }
--- src/utils/DebugHelper.java
+++ src/utils/DebugHelper.java
@@ -4,10 +4,24 @@ import java.text.SimpleDateFormat;
 
 public class DebugHelper {
     static SimpleDateFormat df = new SimpleDateFormat("mm:ss.SSS");
+    public enum printColor{
+        RED("\33[31m"),GREEN("\33[32m"),YELLOW("\33[33m"),BLUE("\33[34m"),PURPLE("\33[35m"),CYAN("\33[36m"),WHITE("\33[37m"),BLACK("\33[30m"),BLANK("\33[m");
+        private String color;
+        printColor(String color){
+            this.color=color;
+        }
+        public String getColor(){
+            return color;
+        }
+    }
     public static <T> void log(T message) {
         System.out.println(df.format(System.currentTimeMillis())+": "+message);
     }
     public static <T> void logErr(T message) {
         System.err.println(df.format(System.currentTimeMillis())+": "+message);
     }
+    public static <T> void logColorful(T message,printColor color) {
+        System.out.println(color.getColor()+df.format(System.currentTimeMillis())+": "+message+printColor.BLANK.getColor());
+    }
+
 }
