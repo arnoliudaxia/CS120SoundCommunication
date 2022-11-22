ce4b181be787adf74a53621c75842644a65eb943
Liuyifei <liuyf7@shanghaitech.edu.cn>
2022-11-23 01:15:41 +0800

    Commuicate

--- src/Tester.java
+++ src/Tester.java
@@ -1,11 +1,25 @@
-import utils.DebugHelper;
+import OSI.Application.SystemController;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class Tester {
     public static void main(final String[] args) throws IOException {
 //        System.out.print("\33[31m 文字"+"\33[m\n");
-        DebugHelper.logColorful("文字", DebugHelper.printColor.RED);
-        DebugHelper.log("文字123");
+        try(ServerSocket socket = new ServerSocket(46569))
+        {
+            Socket client = socket.accept();
+            System.out.println("有连接！");
+            while(true)
+            {
+                OutputStream outputStream = client.getOutputStream();
+                outputStream.write("Hello World".getBytes(StandardCharsets.UTF_8));
+                SystemController.threadBlockTime(1000);
+            }
+
+        }
     }
 }
