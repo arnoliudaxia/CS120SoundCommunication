5ee462d3ac7ceb5eb42bf526c0ff5b1b41bbe75c
Liuyifei <liuyf7@shanghaitech.edu.cn>
2022-11-23 01:37:38 +0800

    发送IP请求

--- src/tasks/part3/ck1/node1.java
+++ src/tasks/part3/ck1/node1.java
@@ -4,29 +4,31 @@ import OSI.Application.MessageSender;
 import OSI.IP.IPv4;
 import OSI.MAC.MACLayer;
 import OSI.Physic.AudioHw;
+import utils.DebugHelper;
 import utils.smartConvertor;
 
 import java.util.ArrayList;
 
 public class node1 {
     public static void main(String[] args) {
-        var timer = System.currentTimeMillis();
         int sendTimes=0;
         IPv4 ip = new IPv4("192.168.123.47");
         AudioHw.initAudioHw();
         MACLayer.initMACLayer();
         MessageSender messager = new MessageSender();
         ArrayList<Integer> information = new ArrayList<>();
+        var timer = System.currentTimeMillis();
         while(sendTimes<10) {
             for (var ipp : ip.ipsegment) {
                 information.addAll(smartConvertor.exactBitsOfNumber(ipp, 8));
             }
             messager.sendBinary(information);
+            MACLayer.macStateMachine.TxPending = true;
             sendTimes++;
             information.clear();
-            while(System.currentTimeMillis()-timer<1000){
-                timer=System.currentTimeMillis();
-            }
+            while(System.currentTimeMillis()-timer<1000){};
+            timer=System.currentTimeMillis();
+            DebugHelper.log("发送下一个ICMP");
         }
 
     }
