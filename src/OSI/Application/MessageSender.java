04a26efe66ac44fc97907190afa68ab973b3da9d
Liuyifei <liuyf7@shanghaitech.edu.cn>
2022-11-26 00:14:19 +0800

    实现发送信息

--- src/OSI/Application/MessageSender.java
+++ src/OSI/Application/MessageSender.java
@@ -6,21 +6,39 @@ import utils.DebugHelper;
 import utils.smartConvertor;
 
 import java.io.FileNotFoundException;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 
 public class MessageSender {
     public void sendMessage(String message) {
-        //TODO
-        DebugHelper.log("随便发一点什么东西");
+        DebugHelper.log("发送字符串");
+        sendBytes(message.getBytes(Charset.defaultCharset()));
+//        var dataInBytes=message.getBytes(Charset.defaultCharset());
+//        ArrayList<Integer> data=new ArrayList<>();
+//        for(byte b:dataInBytes){
+//            data.addAll(smartConvertor.exactBitsOfNumber(b,8));
+//        }
+//        for (byte b : "ç".getBytes()) {
+//            data.addAll(smartConvertor.exactBitsOfNumber(b, 8));
+//        }
+//        MACLayer.macBufferController.trySend(data);
+    }
+    public void sendBytes(byte[] message) {
+        DebugHelper.log("发送bytes");
         ArrayList<Integer> data=new ArrayList<>();
-        data.add(1);
-        data.add(1);
+        for(byte b:message){
+            data.addAll(smartConvertor.exactBitsOfNumber(b,8));
+        }
+        for (byte b : "ç".getBytes()) {
+            data.addAll(smartConvertor.exactBitsOfNumber(b, 8));
+        }
         MACLayer.macBufferController.trySend(data);
     }
     public void sendBinary(ArrayList<Integer> input) {
         DebugHelper.log("发送二进制数据包");
         AudioHw.audioHwG.isPlay=true;
         MACLayer.macBufferController.trySend(input);
+
     }
     public void sendFile(String path) throws FileNotFoundException {
         DebugHelper.log("发送文件: "+path);
