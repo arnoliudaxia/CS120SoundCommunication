cc3d90086049b6679af9940ecdcafbd506bca5a8
Liuyifei <liuyf7@shanghaitech.edu.cn>
2022-11-26 00:14:28 +0800

    Revert "node2回送"
    This reverts commit 9a895fa845b2200f02dc861d1d75c3054f7d9641.

--- src/tasks/part3/ck1/node2.java
+++ src/tasks/part3/ck1/node2.java
@@ -53,7 +53,6 @@ public class node2 {
 
                 byte[] bytes = new byte[1024];
                 int read = client.getInputStream().read(bytes);
-                messager.sendBytes(bytes);
                 DebugHelper.logColorful("收到python", DebugHelper.printColor.GREEN);
                 MACLayer.macStateMachine.TxPending = true;
             }
